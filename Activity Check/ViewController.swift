2aaa5dabd04876a5d9dc2266bdd6e76a053762f9
Justin Bergen <itsniper@gmail.com>
2017-04-17 17:38:09 -0600

    Initial Commit

---
+++ Activity Check/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Activity Check
+//
+//  Created by Justin Bergen on 4/17/17.
+//  Copyright © 2017 Five3 Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
